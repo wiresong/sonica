3c14828649ae80bdcd18ee73ad3fa74a06c84f48
wiresong <wiresong@wiresong.me>
2023-04-02 18:25:58 +0530

    Priliminary setup for github actions and multiplatform support

--- src/audio.ts
+++ src/audio.ts
@@ -1,10 +1,25 @@
 import { ChildProcess, exec } from 'child_process';
+import * as os from 'os';
 import * as vscode from 'vscode';
 
 export class AudioBackend {
   proc: ChildProcess;
   constructor(path: string) {
-    this.proc = exec('audio.exe', { cwd: path });
+    let filename: string;
+    switch (os.platform()) {
+      case 'win32':
+        filename = 'audio.exe';
+        break;
+      case 'darwin':
+        filename = 'audio_mac';
+        break;
+      case 'linux':
+        filename = 'audio_linux';
+        break;
+      default:
+        throw new Error("Unsupported platform");
+    }
+    this.proc = exec(filename, { cwd: path });
     this.proc.stderr?.on('data', vscode.window.showInformationMessage);
     this.proc.stdout?.on('data', vscode.window.showInformationMessage);
   }
